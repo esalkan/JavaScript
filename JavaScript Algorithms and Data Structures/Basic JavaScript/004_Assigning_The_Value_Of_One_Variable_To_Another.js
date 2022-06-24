7ae166034ef8fdcc858ba09cf2a88b61bc025b5f
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-06-24 22:14:14 +0300

    ASSIGN THE VALUE OF ONE VARIABLE TO ANOTHER

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/004_Assigning_The_Value_Of_One_Variable_To_Another.js
@@ -0,0 +1,26 @@
+
+/**
+ * ASSIGN THE VALUE OF ONE VARIABLE TO ANOTHER
+ * >> BİR DEĞİŞKENİN DEĞERİNİ BİR BAŞKA DEĞİŞKENE ATAMA
+ * 
+ * After a value is assigned to a variable using the assignment operator, you can assign the value of that variable to another variable using the assignment operator.
+ * >> Atama operatörü kullanılarak bir değişkene bir değer atandıktan sonra, atama operatörünü kullanarak o değişkenin değerini başka bir değişkene atayabiliriz.
+ */
+
+ var myVar;
+ myVar = 5;
+ 
+ var myNum;
+ myNum = myVar;
+ 
+ /**
+  * The above declares a "myVar" variable with no value, than assigns it the value "5". Next, a variable named "myNum" is declared with no value. Then, the contents of "myVar" (which is "5") is assigned to the variable "myNum". Now, "myNum" also has the value of "5"
+  * >> Yukarıda, değeri olmayan bir "myVar" değişkeni tanımlanır, ona "5" değerini atar. Ardından, "myNum" adlı bir değişken değeri olmayan bir değişken tanımlanır. Ardından, "myVar"ın ("5" olan) değeri "myNum" değişkenine atanır. Şimdi, "myNum" da "5" değerine sahip
+  */
+ 
+ // Assign the contents of "a" to variable "b"
+ // "a"nın içeriğini "b" değişkenine ata
+ var a;
+ a = 7;
+ var b;
+ b = a;
