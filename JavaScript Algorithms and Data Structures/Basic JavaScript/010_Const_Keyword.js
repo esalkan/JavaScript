f3d253bee28feac72346980eebbbc3a1470a2d0b
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-06-27 03:22:56 +0300

    Declare a Read-Only Variable withg the const Keyword

--- JavaScript Algorithms and Data Structures/Basic JavaScript/010_Const_Keyword.js
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/010_Const_Keyword.js
@@ -1,9 +1,12 @@
 /**
  * Declare a Read-Only Variable withg the const Keyword
+ * >> const Anahtar Kelime ile Salt Okunur Değişken Bildirimi
  * 
  * The keyword "let" is not the only new way to declare variables. IN ES6, we can also declare variables using the "const" keyword.
+ * >> "let" anahtar sözcüğü, değişkenleri bildirmenin tek yeni yolu değildir. ES6'da değişkenleri "const" anahtar sözcüğünü kullanarak da bildirebiliriz.
  * 
  * "const" has all the awesome features that "let" has, with the added bonus that variables declared using "const" are read-only. They are a constant value which means that once a variable is assigned with "const", it can not be reassigned.
+ * >> "const", "let"in sahip olduğu tüm harika özelliklere sahiptir ve "const" kullanılarak bildirilen değişkenlerin salt okunur olması ek avantajıyla birlikte. Bunlar sabit bir değerdir, yani bir değişkene "const" atandığında yeniden atama yapılamaz.
  */
 
 const FAV_PET = "Cats";
